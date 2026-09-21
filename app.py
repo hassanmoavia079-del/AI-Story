6963f70847ee160b69acf1c000ad705907c71393
hassanmoavia079-del <hassanmoavia079@gmail.com>
2026-09-21 12:19:22 +0500

    Update app.py

--- app.py
+++ app.py
@@ -158,7 +158,7 @@ def generate_story(prompt):
     client = genai.Client(api_key=api_key)
 
     response = client.models.generate_content(
-        model="gemini-2.5-flash",
+        model="gemini-3.6-flash",
         contents=prompt,
         config=types.GenerateContentConfig(
             temperature=0.9,
